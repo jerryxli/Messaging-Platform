8cbe4eedfbd8df2cb17f314fe5ee28df561b1b08
Tetian Madfouni <z5361722@ad.unsw.edu.au>
2022-02-24 10:59:52 +1100

    Pipeline not running

--- src/auth.py
+++ src/auth.py
@@ -1,7 +1,7 @@
 from src.data_store import data_store
 from src.error import InputError
 
-
+#New push to test if pipeline will run
 def auth_login_v1(email, password):
     store = data_store.get()
     users = store['users']
