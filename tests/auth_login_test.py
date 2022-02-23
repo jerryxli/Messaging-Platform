319bf88e7cf468457467803d858c2e7db745a716
Tetian Madfouni <z5361722@ad.unsw.edu.au>
2022-02-23 22:08:39 +1100

    Added implementation

--- tests/auth_login_test.py
+++ tests/auth_login_test.py
@@ -1,5 +1,7 @@
 from src.auth import auth_login_v1
 from src.auth import auth_register_v1
+from src.error import InputError, AccessError
+import pytest
 
 #Basic Tests
 def test_basic_success():
@@ -7,22 +9,19 @@ def test_basic_success():
     assert auth_login_v1("z55555@unsw.edu.au","passwordlong") == {'auth_user_id': 0}
 
 def test_incorrect_password():
-    auth_register_v1("z55555@unsw.edu.au", "passwordlong", "Jake", "Renzella")
     with pytest.raises(InputError):
         auth_login_v1("z55555@unsw.edu.au","passWRONG")
 
 def test_invalid_email():
-    auth_register_v1("z55555@unsw.edu.au", "passwordlong", "Jake", "Renzella")
     auth_register_v1("z09328373@unsw.edu.au", "passwordlong", "Hayden", "Jacobs")   
     with pytest.raises(InputError):
         auth_login_v1("z1234@unsw.edu.au","passwordlong")
 
 def test_complex_success():
-    auth_register_v1("z55555@unsw.edu.au", "passwordlong", "Jake", "Renzella")
     auth_register_v1("z123@unsw.edu.au", "apples123", "Jacob", "Renzellid")
     auth_register_v1("z12345@unsw.edu.au","bananas&apricots","Apricot","IsNotAFirstName")
     assert auth_login_v1("z55555@unsw.edu.au","passwordlong") == {'auth_user_id': 0}
-    assert auth_login_v1("z12345@unsw.edu.au","bananas&apricots") == {'auth_user_id': 2}
+    assert auth_login_v1("z12345@unsw.edu.au","bananas&apricots") == {'auth_user_id': 3}
     with pytest.raises(InputError):
         auth_login_v1("z123@unsw.edu.au","wrongpasswordboi")
 
