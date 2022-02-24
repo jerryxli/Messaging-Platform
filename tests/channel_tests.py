7c1f0f57981d58ae1fbb8a941ef2689cfa77c0de
Tetian Madfouni <z5361722@ad.unsw.edu.au>
2022-02-24 14:37:44 +1100

    Adding tests

---
+++ tests/channel_tests.py
@@ -0,0 +1,28 @@
+from src.channels import channels_create_v1 
+from src.error import InputError, AccessError
+from src.other import clear_v1
+import pytest
+
+def test_add_public_channel():
+    user_id = auth_register_v1("z55555@unsw.edu.au", "passwordlong", "Jake", "Renzella")
+    channels_create_v1(user_id, 'Jake\'s Room', True) == {'channel_id': 0}
+
+def test_add_private_channel():
+    clear_v1()
+    user_id = auth_register_v1("z55555@unsw.edu.au", "passwordlong", "Jake", "Renzella")
+    channels_create_v1(user_id, 'My Happy place', True) == {'channel_id': 0}
+    channels_create_v1(user_id, 'Woweeee', False) == {'channel_id': 1}
+
+def test_null_name():
+    clear_v1()
+    user_id = auth_register_v1("z55555@unsw.edu.au", "passwordlong", "Jake", "Renzella")
+    with pytest.raises(InputError):
+        channels_create_v1(user_id, '', False)
+
+def test_long_name():
+    clear_v1()
+    user_id = auth_register_v1("z55555@unsw.edu.au", "passwordlong", "Jake", "Renzella")
+    with pytest.raises(InputError):
+        channels_create_v1(user_id, 'abcdefghijklmnopqrstuvwxyzabcdef', False)
+
+
