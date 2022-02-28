3fd34c126ecbfbf0f5be3fdcbdd90b23801a051e
Leo Shi <z5364321@ad.unsw.edu.au>
2022-02-28 22:55:21 +1100

    fix: updated tests, fixed a few errors in my tests

--- tests/channel_join_test.py
+++ tests/channel_join_test.py
@@ -1,51 +1,57 @@
 import pytest
 
 from src.auth import auth_register_v1
-from src.channel import channel_join_v1, is_public_channel
+from src.channel import channel_join_v1
 from src.channels import channels_create_v1
 from src.error import InputError, AccessError
-from src.other import clear_v1, verify_user
+from src.other import clear_v1
 
 @pytest.fixture
 def clear_store():
     clear_v1()
 
-def test_public_channel_true(clear_store):
-    auth_register_v1("hello@unsw.edu.au", "passwordlong", "Hayden", "Smith")
-    channel_id_public = channels_create_v1(0, 'test1', False)
-    assert is_public_channel(channel_id_public) == True    
+@pytest.fixture
+def create_user():
+    user_id = auth_register_v1("z432324@unsw.edu.au", "password", "Name", "Lastname")['auth_user_id']
+    return user_id  
 
-def test_public_channel_false(clear_store):
-    auth_register_v1("hello@unsw.edu.au", "passwordlong", "Hayden", "Smith")
-    channel_id_private = channels_create_v1(0, 'test2', True)
+@pytest.fixture
+def create_user2():
+    user_id2 = auth_register_v1("z432325@unsw.edu.au", "password1", "Name1", "Lastname1")['auth_user_id']
+    return user_id2 
+
+def test_private_channel(clear_store, create_user, create_user2):
+    user_id = create_user
+    user_id2 = create_user2
+    channels = channels_create_v1(user_id, 'test2', False)
     with pytest.raises(AccessError):
-        is_public_channel(channel_id_private)
+        channel_join_v1(user_id2, channels['channel_id']) 
 
-def test_successfully_joined_channel():
-    auth_register_v1("hello@unsw.edu.au", "passwordlong", "Hayden", "Smith")
-    channels = channels_create_v1(0, 'test2', False)
-    channel_join_v1(0, channels['channel_id']) 
-    assert len(channels['users']) == 1
+def test_successfully_joined_channel(clear_store, create_user, create_user2):
+    user_id = create_user
+    user_id2 = create_user2
+    channels = channels_create_v1(user_id, 'test2', True)
+    expected_outcome = None
+    assert expected_outcome == channel_join_v1(user_id2, channels['channel_id']) 
 
-def test_successfully_joined_channel2():
+def test_successfully_joined_channel2(clear_store):
     auth_register_v1("hello@unsw.edu.au", "passwordlong", "Hayden", "Smith")
     auth_register_v1("z09328373@unsw.edu.au", "passwordlong", "Hayden", "Jacobs")
     auth_register_v1("z55555@unsw.edu.au", "passwordlong", "Jake", "Renzella")
-    channels = channels_create_v1(0, 'test2', False)
-    channel_join_v1(0, channels['channel_id'])
-    channel_join_v1(1, channels['channel_id']) 
-    channel_join_v1(2, channels['channel_id']) 
-    assert len(channels['users']) == 3
+    channels = channels_create_v1(0, 'test2', True)
+    expected_outcome = None
+    assert expected_outcome == channel_join_v1(1, channels['channel_id']) 
+    assert expected_outcome == channel_join_v1(2, channels['channel_id']) 
+    
 
-def test_channel_doesnt_exist():
-    auth_register_v1("z09328373@unsw.edu.au", "passwordlong", "Hayden", "Jacobs")
+def test_channel_doesnt_exist(clear_store, create_user, create_user2):
+    user_id = create_user
     with pytest.raises(InputError):
-        channel_join_v1(0, 0)    
+        channel_join_v1(user_id, 0)    
 
-def test_user_already_in_channel():
-    auth_register_v1("hello@unsw.edu.au", "passwordlong", "Hayden", "Smith")
-    channels = channels_create_v1(0, 'test2', False)
-    channel_join_v1(0, channels['channel_id']) 
+def test_user_already_in_channel(clear_store, create_user, create_user2):
+    user_id = create_user
+    channels = channels_create_v1(user_id, 'test2', True)
     with pytest.raises(InputError):
-        channel_join_v1(0, channels['channel_id'])
+        channel_join_v1(user_id, channels['channel_id'])
 
