884d10a3d718175de1fcd9afd0676344f0da30c4
Jacqueline Chen <jacquelinechen555@gmail.com>
2022-03-02 02:49:09 +1100

    Feat: Added the channel_details_v1 function

--- src/channel.py
+++ src/channel.py
@@ -1,29 +1,78 @@
+from src.data_store import data_store
+from src.error import AccessError, InputError
+
+
 def channel_invite_v1(auth_user_id, channel_id, u_id):
     return {
     }
 
+# given a channel id, return a channel dictionary
+def get_channel(id):
+    store = data_store.get()
+    channels = store['channels']
+    for channel in channels:
+        if int(id) == channel['channel_id']:
+            return channel
+    return None
+
+# check if the user is already in the channel
+def check_user_in_channel(user_id, channel):
+    for member in channel['users']:
+        if user_id == member:
+            return True
+    return False
+
+# Append the user info into the respective lists
+def get_user_details(auth_user_id):
+    # Accesses the data_store to get the objects
+    store = data_store.get()
+    users = store['users']
+    
+    # Loops through users to append info
+    for user in users:
+        if auth_user_id == user['auth_user_id']:
+            details = {}
+            details['u_id'] = user['auth_user_id']
+            details['email'] = user['email']
+            details['name_first'] = user['name_first']
+            details['name_last'] = user['name_last']
+            details['handle'] = user['handle']
+    return details
+
+
 def channel_details_v1(auth_user_id, channel_id):
-    return {
-        'name': 'Hayden',
-        'owner_members': [
-            {
-                'u_id': 1,
-                'email': 'example@gmail.com',
-                'name_first': 'Hayden',
-                'name_last': 'Jacobs',
-                'handle_str': 'haydenjacobs',
-            }
-        ],
-        'all_members': [
-            {
-                'u_id': 1,
-                'email': 'example@gmail.com',
-                'name_first': 'Hayden',
-                'name_last': 'Jacobs',
-                'handle_str': 'haydenjacobs',
-            }
-        ],
-    }
+    '''
+    Returns the details of a channel in the format:
+    { name: , is_public: , owner_members: [], all_members: [], }
+    '''
+    # Checks for Input error: when the channel_id does not exist
+    if get_channel(channel_id) == None:
+        raise(InputError)
+    channel = get_channel(channel_id)
+    # Checks for Access error: when the user is not a member of the channel
+    if check_user_in_channel(auth_user_id, channel) == False:
+        raise(AccessError)
+    
+    # Create a blank dictionary that will contain the channel details
+    channel_details = {}
+
+    # Add the details into the channel_details dictionary
+    channel_details['Name'] = channel['Name']
+    channel_details['is_public'] = channel['is_public']
+    channel_details['owner_members'] = []
+    channel_details['all_members'] = []
+
+    # Append the user info into the owner_members list
+    owner_members = channel_details['owner_members']
+    for owner in channel['channel_owners']:
+        owner_members.append(get_user_details(owner))
+
+    # Append the members info into all_members list
+    all_members = channel_details['all_members']
+    for member in channel['channel_members']: 
+        all_members.append(get_user_details(member))
+
+    return channel_details
 
 def channel_messages_v1(auth_user_id, channel_id, start):
     return {
