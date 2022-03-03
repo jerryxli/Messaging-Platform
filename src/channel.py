e33c1b4507f0d44536691a92d42331003964665d
Jerry Li <z5362290@ad.unsw.edu.au>
2022-03-04 01:17:58 +1100

    tried fixing a bug

--- src/channel.py
+++ src/channel.py
@@ -2,13 +2,29 @@ from src.data_store import data_store
 from src.error import InputError, AccessError
 from src.other import verify_user
 
+def check_user_in_channel(auth_user_id:int, channel:dict)->bool:
+    '''
+    Checks whether a user is in a channel or not
+
+    Arguments:
+        user_id (int) - the id of the user
+        channel (dict) - the channel to check
+    
+    Returns:
+        A boolean, true if the user is in the channel, false if not
+
+    '''
+    if auth_user_id in channel['channel_members']:
+        return True
+    else:
+        return False
+
 def channel_invite_v1(auth_user_id, channel_id, u_id):
     if verify_user(auth_user_id) is False:
         raise(AccessError)
     if verify_user(u_id) is False:
         raise InputError("u_id does not refer to a valid user")
     
-
     store = data_store.get()
     channel = None
     channels = store['channels']
@@ -88,23 +104,8 @@ def channel_messages_v1(auth_user_id, channel_id, start):
 
 
 
-def check_user_in_channel(auth_user_id:int, channel:dict)->bool:
-    '''
-    Checks whether a user is in a channel or not
 
-    Arguments:
-        user_id (int) - the id of the user
-        channel (dict) - the channel to check
-    
-    Returns:
-        A boolean, true if the user is in the channel, false if not
 
-    '''
-    for user in channel['channel_members']:
-        print(user)
-        print(user['u_id'])
-    ids = [user['u_id'] for user in channel['channel_members']]
-    return bool(auth_user_id in ids)
 
 
 def channel_join_v1(auth_user_id, channel_id):
