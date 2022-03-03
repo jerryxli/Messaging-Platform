88dc2019e16fddf8a2ec327dd74cdc4e880274b6
Leo Shi <z5364321@ad.unsw.edu.au>
2022-03-04 00:20:29 +1100

    fix: fixed channel_messages_v1 so that it passes the tests we have made

--- src/channel.py
+++ src/channel.py
@@ -28,25 +28,33 @@ def channel_details_v1(auth_user_id, channel_id):
         ],
     }
 
+# check if the user is already in the channel
+def check_user_in_channel(user_id, channel):
+    if user_id in channel['channel_members']:
+        return True
+    else:
+        return False
+    
 def channel_messages_v1(auth_user_id, channel_id, start):
     start = int(start)
     page_threshold = 50
-
-    # locate user_id, if not found, return AccessError
-    user = None
     store = data_store.get()
+
+    user = None
     users = store['users']
     if auth_user_id in users.keys():
-        user = users[user_id]
-    else:
-        raise AccessError("channel_id is valid and the authorised user is not a memeber of the channel")
-    # locate channel_id, if not found, return InputError
+        user = users[auth_user_id]
+
     channel = None
     channels = store['channels']
     if channel_id in channels.keys():
         channel = channels[channel_id]
-    else:
+
+    if channel == None:
         raise InputError("channel_id does not refer to a valid channel")
+    if check_user_in_channel(auth_user_id, channel) == False:
+        raise AccessError("channel_id is valid and the authorised user is not a member of the channel")
+    
     # determine if start is greater than total number of messages, if so, return InputError
     if start > len(channel['messages']):
         raise InputError("start is greater than the total number of messages in the channel")
@@ -54,16 +62,7 @@ def channel_messages_v1(auth_user_id, channel_id, start):
     messages = []
     not_displayed = list(reversed(channel['messages']))[start:]
     end = -1 if len(messages) == len(not_displayed) else start + page_threshold
-
     return {'messages': messages, 'start': start, 'end': end}
-
-
-# check if the user is already in the channel
-def check_user_in_channel(user_id, channel):
-    if user_id in channel['channel_members']:
-        return True
-    else:
-        return False
 
 def channel_join_v1(auth_user_id, channel_id):
     channel = None
