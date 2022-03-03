36d9aecc021324d9ea390956092666ad5d534594
Jerry Li <z5362290@ad.unsw.edu.au>
2022-03-03 14:37:30 +1100

    started implementing function

--- src/channel.py
+++ src/channel.py
@@ -1,6 +1,22 @@
+from src.data_store import data_store
+from src.error import InputError, AccessError
+
 def channel_invite_v1(auth_user_id, channel_id, u_id):
-    return {
-    }
+    channel = get_channel(channel_id)
+    if #auth_user_id is invalid
+        raise(AccessError) 
+    
+    if #u_id is invalid user
+        raise InputError("u_id does not refer to a valid user")
+    if check_user_in_channel(u_id, channel) == True:
+        raise InputError("u_id reders to a user who is already a member of the channel")
+    if channel is None:
+        raise InputError("channel_id does not refer to a valid channel")
+    elif check_user_in_channel(auth_user_id, channel) == False:
+        raise(AccessError)
+
+    channel_join_v1(u_id, channel_id)
+    
 
 def channel_details_v1(auth_user_id, channel_id):
     return {
@@ -42,3 +58,21 @@ def channel_messages_v1(auth_user_id, channel_id, start):
 def channel_join_v1(auth_user_id, channel_id):
     return {
     }
+
+#---------------------Functions for Testing---------------------#
+# given a channel id, return a channel dictionary
+def get_channel(id):
+    store = data_store.get()
+    channels = store['channels']
+    for channel in channels:
+        if int(id) == channel['channel_id']:
+            return channel
+    return None
+
+# check if the user is in the channel
+def check_user_in_channel(user_id, channel):
+    for member in channel['users']:
+        if user_id == member:
+            return True
+    return False
+#---------------------Functions for Testing---------------------#
