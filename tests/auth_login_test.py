501400e834fce3d9bb709e3451d77a99f70f419f
Samuel Bell <z5362604@ad.unsw.edu.au>
2022-03-18 18:24:16 +1100

    fix: Fixed style issue in auth_login_test

--- tests/auth_login_test.py
+++ tests/auth_login_test.py
@@ -25,20 +25,20 @@ def test_basic_success(clear_store):
     assert is_valid_dictionary_output(login_request.json(), {"token": str, "auth_user_id": int})
 
 def test_incorrect_password(clear_store):
-    register_request = requests.post(REGISTER_URL, json={"email":"z55555@unsw.edu.au", "password":"passwordlong", "name_first":"Jake", "name_last":"Renzella"})
+    requests.post(REGISTER_URL, json={"email":"z55555@unsw.edu.au", "password":"passwordlong", "name_first":"Jake", "name_last":"Renzella"})
     login_request = requests.post(LOGIN_URL, json={"email":"z55555@unsw.edu.au", "password":"passWRONG"})
     assert login_request.status_code != 200
 
 def test_invalid_email(clear_store):
-    register_request = requests.post(REGISTER_URL, json={"email":"z09328373@unsw.edu.au", "password":"passwordlong", "name_first":"Hayden", "name_last":"Jacobs"})
+    requests.post(REGISTER_URL, json={"email":"z09328373@unsw.edu.au", "password":"passwordlong", "name_first":"Hayden", "name_last":"Jacobs"})
     login_request = requests.post(LOGIN_URL, json={"email":"z1234@unsw.edu.au", "password":"passWRONG"})
     assert login_request.status_code != 200
 
 def test_complex_success(clear_store):
-    register_request_0 = requests.post(REGISTER_URL, json={"email":"z55555@unsw.edu.au", "password":"passwordlong", "name_first":"Jake", "name_last":"Renzella"})
-    register_request_1 = requests.post(REGISTER_URL, json={"email":"z09328373@unsw.edu.au", "password":"passwordlong", "name_first":"Hayden", "name_last":"Jacobs"})
-    register_request_2 = requests.post(REGISTER_URL, json={"email":"z123@unsw.edu.au", "password":"apples123", "name_first":"Jakob", "name_last":"Renzellid"})
-    register_request_3 = requests.post(REGISTER_URL, json={"email":"z12345@unsw.edu.au", "password":"bananas&apricots", "name_first":"Apricot", "name_last":"IsNotAFirstName"})
+    requests.post(REGISTER_URL, json={"email":"z55555@unsw.edu.au", "password":"passwordlong", "name_first":"Jake", "name_last":"Renzella"})
+    requests.post(REGISTER_URL, json={"email":"z09328373@unsw.edu.au", "password":"passwordlong", "name_first":"Hayden", "name_last":"Jacobs"})
+    requests.post(REGISTER_URL, json={"email":"z123@unsw.edu.au", "password":"apples123", "name_first":"Jakob", "name_last":"Renzellid"})
+    requests.post(REGISTER_URL, json={"email":"z12345@unsw.edu.au", "password":"bananas&apricots", "name_first":"Apricot", "name_last":"IsNotAFirstName"})
 
     login_request_0 = requests.post(LOGIN_URL, json={"email": "z55555@unsw.edu.au", "password": "passwordlong"})
     assert is_valid_dictionary_output(login_request_0.json(), {"token": str, "auth_user_id": int})
