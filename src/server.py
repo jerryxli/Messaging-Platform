c2245167c35adce26d05497e7c1a6c40ae6a92a6
Jacqueline Chen <jacquelinechen555@gmail.com>
2022-03-22 01:56:25 +1100

    Added the route for channel_addowner

--- src/server.py
+++ src/server.py
@@ -8,7 +8,7 @@ from flask_cors import CORS
 from src.error import AccessError, InputError
 from src import config
 from src.other import clear_v1, user_id_from_JWT
-from src.channel import channel_details_v1, channel_join_v1, channel_leave_v1
+from src.channel import channel_details_v1, channel_join_v1, channel_leave_v1, channel_messages_v1, channel_addowner_v1
 from src.channels import channels_create_v1, channels_list_v1
 from src.auth import auth_login_v1, auth_logout_v1, auth_register_v1, is_valid_JWT
 from src.user import user_profile_v1, user_setemail_v1, user_setname_v1
@@ -155,6 +155,28 @@ def handle_channel_leave():
         raise AccessError("JWT no longer valid")
     user_id = user_id_from_JWT(user_token)
     channel_leave_v1(user_id, channel_id)
+    return {}
+
+@APP.route("/channel/messages/v2", methods = ['GET'])
+def handle_channel_messages():
+    user_token = request.args.get('token')
+    channel_id = int(request.args.get('channel_id'))
+    start = int(request.args.get('start'))
+    if not is_valid_JWT(user_token):
+        raise AccessError("JWT no longer valid")
+    user_id = user_id_from_JWT(user_token)
+    return channel_messages_v1(user_id, channel_id, start)
+    
+@APP.route("/channel/addowner/v1", methods = ["POST"])
+def handle_channel_addowner():
+    request_data = request.get_json()
+    user_token = request_data['token']
+    channel_id = int(request_data['channel_id'])
+    u_id = int(request_data['u_id'])
+    if not is_valid_JWT(user_token):
+        raise AccessError("JWT no longer valid")
+    user_id = user_id_from_JWT(user_token)
+    channel_addowner_v1(user_id, channel_id, u_id)
     return {}
 
 #### NO NEED TO MODIFY BELOW THIS POINT
