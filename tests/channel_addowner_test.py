7a2eb4d9f4882e51c70096f3f0e59a034c10fe5a
Jacqueline Chen <jacquelinechen555@gmail.com>
2022-03-22 03:26:52 +1100

    Added changes to allow for global_owners to add owners without being in the channel

--- tests/channel_addowner_test.py
+++ tests/channel_addowner_test.py
@@ -93,7 +93,7 @@ def test_global_owner_add_themselves(clear_store, create_user, create_user2):
     user_token_1 = create_user['token']
     user_token_2 = create_user2['token']
     user_id = create_user['auth_user_id']
-    channel_id_2 = requests.post(CREATE_URL, json = {'token': user_token_2, 'name': 'kitchen', 'is_public': False}).json()['channel_id']
+    channel_id_2 = requests.post(CREATE_URL, json = {'token': user_token_2, 'name': 'kitchen', 'is_public': True}).json()['channel_id']
     join_request = requests.post(JOIN_URL, json = {'token': user_token_1, 'channel_id': channel_id_2})
     response = requests.post(ADDOWNER_URL, json = {'token': user_token_1, 'channel_id': channel_id_2, 'u_id': user_id})
     channel_details = requests.get(DETAILS_URL, params = {'token': user_token_1, 'channel_id': channel_id_2}).json()
