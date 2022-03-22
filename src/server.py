21cce0e02eb481936002b5602a1d937fe10a9306
Leo Shi <leo.y.shi@student.unsw.edu.au>
2022-03-22 22:16:40 +1100

    Update server.py

--- src/server.py
+++ src/server.py
@@ -7,11 +7,10 @@ from flask_cors import CORS
 from src.error import AccessError, InputError
 from src import config
 from src.other import clear_v1, user_id_from_JWT
-from src.channel import channel_details_v1, channel_join_v1, channel_leave_v1, channel_messages_v1, channel_addowner_v1, channel_removeowner_v1
+from src.channel import channel_details_v1, channel_join_v1, channel_leave_v1, channel_messages_v1
 from src.channels import channels_create_v1, channels_list_v1, channels_listall_v1
 from src.auth import auth_login_v1, auth_logout_v1, auth_register_v1, is_valid_JWT
 from src.user import user_profile_v1, user_setemail_v1, user_setname_v1
-from src.message import message_send_v1
 
 
 def quit_gracefully(*args):
@@ -178,17 +177,6 @@ def handle_channel_leave():
     channel_leave_v1(user_id, channel_id)
     return {}
 
-@APP.route("/channel/addowner/v1", methods = ["POST"])
-def handle_channel_addowner():
-    request_data = request.get_json()
-    user_token = request_data['token']
-    channel_id = int(request_data['channel_id'])
-    u_id = int(request_data['u_id'])
-    if not is_valid_JWT(user_token):
-        raise AccessError("JWT no longer valid")
-    user_id = user_id_from_JWT(user_token)
-    channel_addowner_v1(user_id, channel_id, u_id)
-    return {}
 
 @APP.route("/channel/messages/v2", methods=['GET'])
 def handle_channel_messages():
@@ -199,33 +187,7 @@ def handle_channel_messages():
         raise AccessError("JWT no longer valid")
     user_id = user_id_from_JWT(user_token)
     return channel_messages_v1(user_id, channel_id, start)
-
-    
-@APP.route("/channel/removeowner/v1", methods = ["POST"])
-def handle_channel_removeowner():
-    request_data = request.get_json()
-    user_token = request_data['token']
-    channel_id = int(request_data['channel_id'])
-    u_id = int(request_data['u_id'])
-    if not is_valid_JWT(user_token):
-        raise AccessError("JWT no longer valid")
-    user_id = user_id_from_JWT(user_token)
-    channel_removeowner_v1(user_id, channel_id, u_id)
-    return {}
-
-
-# NO NEED TO MODIFY BELOW THIS POINT
-
-@APP.route("/message/send/v1", methods = ['POST'])
-def handle_message_send():
-    request_data = request.get_json()
-    user_token = request_data['token']
-    channel_id = int(request_data['channel_id'])
-    message = request_data['message']
-    if not is_valid_JWT(user_token):
-        raise AccessError("JWT no longer valid")
-    user_id = user_id_from_JWT(user_token)
-    return message_send_v1(user_id, channel_id, message)    
+  
 
 if __name__ == "__main__":
     signal.signal(signal.SIGINT, quit_gracefully)  # For coverage
