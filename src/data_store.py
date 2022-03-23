f7d78bfd99f93f24d9b6513a32313aea4789f569
Samuel Bell <samuel.bell@student.unsw.edu.au>
2022-03-23 12:30:45 +1100

    Reversed a change which stopped state from being loaded

--- src/data_store.py
+++ src/data_store.py
@@ -55,9 +55,9 @@ class Datastore:
     def __init__(self):
         print("Creating Data Store")
         try:
-            self.__store = initial_object
-        except:
             self.__store = unpickle_and_load()
+        except:
+            self.__store = initial_object
 
     def get(self):
         return self.__store
