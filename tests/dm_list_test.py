b2e5d336237fa94a9940d54191580769a09af0a3
Jerry Li <z5362290@ad.unsw.edu.au>
2022-03-24 00:13:59 +1100

    fixed dm_name

--- tests/dm_list_test.py
+++ tests/dm_list_test.py
@@ -46,35 +46,32 @@ def test_list_not_in_any_dms(clear_store, create_user):
     assert response.status_code == 200
     assert response_data == {'dms': [] }
 
-def test_list_in_one_dm(clear_store, create_user):
+def test_list_in_one_dm(clear_store, create_user, create_user2):
     user_token = create_user['token']
     user_id_2 = create_user2['auth_user_id']
 
     response0 = requests.post(DM_CREATE_URL, json = {'token': user_token, 'u_ids': [user_id_2]})
-    dm_name = response0.json()['name'] # BUT DM CREATE ONLY RETURNS DM_ID NOT NAME
     dm_id = response0.json()['dm_id']
 
     response = requests.get(DM_LIST_URL, params = {'token': user_token})
     response_data = response.json()
     assert response.status_code == 200
-    assert response_data == {'dms': [{'dm_id': dm_id, 'name': dm_name}] }
+    assert response_data == {'dms': [{'dm_id': dm_id, 'name': "name1lastname1, name2lastname2"}] }
 
-def test_list_in_multiple_dm(clear_store, create_user):
+def test_list_in_multiple_dm(clear_store, create_user, create_user2, create_user3):
     user_token = create_user['token']
     user_id_2 = create_user2['auth_user_id']
     user_id_3 = create_user3['auth_user_id']
 
     response_data_1 = requests.post(DM_CREATE_URL, json = {'token': user_token, 'u_ids': [user_id_2]}).json()
-    dm_name_1 = response_data_1['name'] # BUT DM CREATE ONLY RETURNS DM_ID NOT NAME
     dm_id_1 = response_data_1['dm_id']
     response_data_2 = requests.post(DM_CREATE_URL, json = {'token': user_token, 'u_ids': [user_id_2, user_id_3]}).json()
-    dm_name_2 = response_data_2['name'] # BUT DM CREATE ONLY RETURNS DM_ID NOT NAME
     dm_id_2 = response_data_2['dm_id']
 
     response = requests.get(DM_LIST_URL, params = {'token': user_token})
     response_data = response.json()
     assert response.status_code == 200
-    assert response_data == {'dms': [{'dm_id': dm_id_1, 'name': dm_name_1}, {'dm_id': dm_id_2, 'name': dm_name_2}]}
+    assert response_data == {'dms': [{'dm_id': dm_id_1, 'name': "name1lastname1, name2lastname2"}, {'dm_id': dm_id_2, 'name': "name1lastname1, name2lastname2, name3lastname3"}]}
 
 def test_invalid_token(clear_store, create_user):
     user_token = create_user['token']
