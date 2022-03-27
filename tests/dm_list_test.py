275d56b446c71bc1fac56bd4445bfd24cc815d49
Tetian Madfouni <z5361722@ad.unsw.edu.au>
2022-03-27 14:05:59 +1100

    fix: edited pylint issue v2

--- tests/dm_list_test.py
+++ tests/dm_list_test.py
@@ -73,7 +73,6 @@ def test_not_in_dm(clear_store, create_user, create_user2):
     user_id_2 = create_user2['auth_user_id']
     response_data_1 = requests.post(DM_CREATE_URL, json = {'token': user_token, 'u_ids': [user_id_2]}).json()
     dm_id_1 = response_data_1['dm_id']
-    response_data_2 = requests.post(DM_CREATE_URL, json = {'token': user_token, 'u_ids': []}).json()
     response = requests.get(DM_LIST_URL, params = {'token': create_user2['token']}).json()
     assert response == {'dms': [{'dm_id': dm_id_1, 'name': "name1lastname1, name2lastname2"}]}
     
