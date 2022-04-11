ee153d9925062e63ccdc31ef080056a8d94942d6
Jacqueline Chen <jacquelinechen555@gmail.com>
2022-04-11 23:38:07 +1000

    Added functionality for notifications

---
+++ src/notifications.py
@@ -0,0 +1,39 @@
+"""
+Notifications
+Filename: notifications.py
+
+Author: Jacqueline Chen (z5360310)
+Created: 11.04.2022
+
+Description: Allows the user to receive notifiations when they are tagged in a message,
+when a user reacts to their message and when they are added to a channel or dm.
+"""
+
+from time import time
+from src.data_store import data_store
+from src.error import InputError, AccessError
+import src.other as other
+
+def notifications_get_v1(auth_user_id: int) -> dict:
+    """
+    Gets the 20 most recent notifcations for the user
+
+    Exceptions:
+        None
+
+    Arguments:
+        auth_user_id (int)  - The id of the user
+
+    Return Value:
+        Returns { 'notifications' } on successful creation
+    """
+    notifications = list()
+    store = data_store.get()
+    notifications = store['notifications']
+
+    user_notifications = notifications[auth_user_id]
+    if len(user_notifications) > 20:
+        reversed_list = user_notifications[-20:]
+        return {'notifications': reversed_list.reverse()}
+    
+    return {'notifications': user_notifications.reverse()}
