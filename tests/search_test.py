3ca2971de85b0e28ce00ce99e637e6c864163bc2
Jacqueline Chen <jacquelinechen555@gmail.com>
2022-04-12 22:50:44 +1000

    Fixed: fixed test errors

--- tests/search_test.py
+++ tests/search_test.py
@@ -1,4 +1,5 @@
 from src.config import url
+from time import time
 import pytest
 import requests
 import src.other as other
@@ -8,7 +9,7 @@ def clear_store():
     requests.delete(f"{url}/clear/v1", json={})
 
 @pytest.fixture
-def create_user():
+def create_user1():
     user_input = {'email': "z432324@unsw.edu.au", 'password': "badpassword123", 'name_first': "Twix", 'name_last': "Fix"}
     request_data = requests.post(other.REGISTER_URL, json=user_input)
     user_info = request_data.json()
@@ -23,19 +24,16 @@ def long_string():
 
 def test_message_contains_query(clear_store, create_user1):
     user_token = create_user1['token']
-    user_id = create_user1['auth_user_id']
     channel_id = requests.post(other.CHANNELS_CREATE_URL, json={'token': user_token, 'name': 'Channel!', 'is_public': True}).json()['channel_id']
-    requests.post(other.MESSAGE_SEND_URL, json={'token': user_token, 'channel_id': channel_id, 'message': "Query word hello"})
-    requests.post(other.MESSAGE_SEND_URL, json={'token': user_token, 'channel_id': channel_id, 'message': "hello world"})
-    requests.post(other.MESSAGE_SEND_URL, json={'token': user_token, 'channel_id': channel_id, 'message': "bye bye hello hello"})
+    requests.post(other.MESSAGE_SEND_URL, json={'token': user_token, 'channel_id': channel_id, 'message': "Query word hello"}).json()['message_id']
+    channel_messages = requests.get(other.CHANNEL_MESSAGES_URL, params={'token': user_token, 'channel_id': channel_id, 'start': 0}).json()
     response = requests.get(other.SEARCH_URL, params={'token': user_token, 'query_str': "hello"})
-    expected_output = {'messages': ["Query word hello", "hello world", "bye bye hello hello"]}
-    assert response.json() == expected_output
+    expected_output = channel_messages['messages']
+    assert response.json()['messages'] == expected_output
     assert response.status_code == 200
 
 def test_message_do_not_contain_query(clear_store, create_user1):
     user_token = create_user1['token']
-    user_id = create_user1['auth_user_id']
     channel_id = requests.post(other.CHANNELS_CREATE_URL, json={'token': user_token, 'name': 'Channel!', 'is_public': True}).json()['channel_id']
     requests.post(other.MESSAGE_SEND_URL, json={'token': user_token, 'channel_id': channel_id, 'message': "hello"})
     response = requests.get(other.SEARCH_URL, params={'token': user_token, 'query_str': "word"})
@@ -45,14 +43,15 @@ def test_message_do_not_contain_query(clear_store, create_user1):
 
 def test_query_too_long(clear_store, create_user1, long_string):
     user_token = create_user1['token']
-    user_id = create_user1['auth_user_id']
-    channel_id = requests.post(other.CHANNELS_CREATE_URL, json={'token': user_token, 'name': 'Channel!', 'is_public': True}).json()['channel_id']
+    create_user1['auth_user_id']
+    requests.post(other.CHANNELS_CREATE_URL, json={'token': user_token, 'name': 'Channel!', 'is_public': True}).json()['channel_id']
     response = requests.get(other.SEARCH_URL, params={'token': user_token, 'query_str': long_string})
     assert response.status_code == 400
 
 def test_invalid_user(clear_store, create_user1):
-    user_token = create_user['token']
+    user_token = create_user1['token']
     channel_id = requests.post(other.CHANNELS_CREATE_URL, json={'token': user_token, 'name': 'Channel!', 'is_public': True}).json()['channel_id']
+    requests.post(other.MESSAGE_SEND_URL, json={'token': user_token, 'channel_id': channel_id, 'message': "Hello"})
     requests.post(other.LOGOUT_URL, json={'token': user_token})
-    request_data = requests.get(other.CHANNEL_DETAILS_URL, params={'channel_id': channel_id}, json={'token': user_token})
+    request_data = requests.get(other.SEARCH_URL, params={'token': user_token, 'query_str': "Hello"})
     assert request_data.status_code == 403
