2b496b92daea5fffe50bd9ea1fd5fb5dc413c597
Jacqueline Chen <jacquelinechen555@gmail.com>
2022-04-13 22:45:33 +1000

    Fixed bug with sendlater test

--- tests/notifications_get_test.py
+++ tests/notifications_get_test.py
@@ -78,13 +78,13 @@ def test_sendlater_tag(clear_store, create_user1):
     requests.post(other.MESSAGE_SENDLATER_URL, json={'token': user_token, 'channel_id': channel_id, 'message': "Tag you later @twixfix", 'time_sent': time() + 1})
     sleep(2)
     response = requests.get(other.NOTIFICATIONS_GET_URL, params={'token': user_token})
-    expected_output = [{'channel_id': channel_id, 'dm_id': -1, 'notification_message': 'twixfix tagged you in Cool Channel: Tag you later @twixfix'}]
+    expected_output = [{'channel_id': channel_id, 'dm_id': -1, 'notification_message': 'twixfix tagged you in Cool Channel: Tag you later @twixf'}]
     assert response.json()['notifications'] == expected_output
     assert response.status_code == 200
 
 def test_sendlaterdm_tag(clear_store, create_user1):
     user_token = create_user1['token']
-    dm_id = requests.post(other.DM_CREATE_URL, json={'token': user_token, 'u_id': []}).json()['channel_id']
+    dm_id = requests.post(other.DM_CREATE_URL, json={'token': user_token, 'u_ids': []}).json()['dm_id']
     requests.post(other.MESSAGE_SENDLATERDM_URL, json={'token': user_token, 'dm_id': dm_id, 'message': "Tag you later @twixfix", 'time_sent': time() + 1})
     sleep(2)
     response = requests.get(other.NOTIFICATIONS_GET_URL, params={'token': user_token})
