c20d901d9e05fd5dfc4b6be1ba65c7ad3db22172
Jerry Li <z5362290@ad.unsw.edu.au>
2022-04-14 15:28:18 +1000

    fixed return type

--- tests/standup_start_test.py
+++ tests/standup_start_test.py
@@ -1,7 +1,7 @@
 from src.config import url
 import requests
 import pytest
-import time
+from time import time, sleep
 import src.other as other
 
 @pytest.fixture
@@ -10,7 +10,7 @@ def clear_store():
 
 @pytest.fixture
 def create_user():
-    user_input = {'email': "z4323234@unsw.edu.au", 'password': "Password1", 'name_first': "Name1", 'name_last': "LastName1"}
+    user_input = {'email': "z432324@unsw.edu.au", 'password': "badpassword123", 'name_first': "Twix", 'name_last': "Fix"}
     request_data = requests.post(other.REGISTER_URL, json=user_input)
     user_info = request_data.json()
     return user_info
@@ -24,40 +24,40 @@ def create_user2():
 
 def test_success_start(clear_store, create_user):
     user_token = create_user['token']
-    channel_id = requests.post(other.CHANNELS_CREATE_URL, json={'token': user_token, 'name': 'My Channel!', 'is_public': True}).json()['channel_id']
-    response = requests.post(other.STANDUP_START_URL, json={'token': user_token, 'channel_id': channel_id, 'length': 30})
-    time_start = time.time()
-    time_finish = time_start + 30
-    
+    channel_id = requests.post(other.CHANNELS_CREATE_URL, json={'token': user_token, 'name': 'Channel!', 'is_public': True}).json()['channel_id']
+    response = requests.post(other.STANDUP_START_URL, json={'token': user_token, 'channel_id': channel_id, 'length': 10})
+    time_start = time()
+    time_finish = int(time_start + 10)
+    assert channel_id == 0
     assert response.status_code == 200
     response_data = response.json()
-    assert response_data == time_finish
+    assert response_data == {'time_finish': time_finish}
 
 
 def test_negative_length(clear_store, create_user):
     user_token = create_user['token']
     channel_id = requests.post(other.CHANNELS_CREATE_URL, json={'token': user_token, 'name': 'My Channel!', 'is_public': True}).json()['channel_id']
-    response = requests.post(other.STANDUP_START_URL, json={'token': user_token, 'channel_id': channel_id, 'length': -30})
+    response = requests.post(other.STANDUP_START_URL, json={'token': user_token, 'channel_id': channel_id, 'length': -10})
     assert response.status_code == 400
 
 def test_standup_already_running(clear_store, create_user):
     user_token = create_user['token']
     channel_id = requests.post(other.CHANNELS_CREATE_URL, json={'token': user_token, 'name': 'My Channel!', 'is_public': True}).json()['channel_id']
-    response1 = requests.post(other.STANDUP_START_URL, json={'token': user_token, 'channel_id': channel_id, 'length': 30})
+    response1 = requests.post(other.STANDUP_START_URL, json={'token': user_token, 'channel_id': channel_id, 'length': 10})
     assert response1.status_code == 200
 
-    response2 = requests.post(other.STANDUP_START_URL, json={'token': user_token, 'channel_id': channel_id, 'length': 30})
+    response2 = requests.post(other.STANDUP_START_URL, json={'token': user_token, 'channel_id': channel_id, 'length': 10})
     assert response2.status_code == 400
     
 def test_invalid_channel(clear_store, create_user):
     user_token = create_user['token']
-    response = requests.post(other.STANDUP_START_URL, json={'token': user_token, 'channel_id': 0, 'length': 30})
+    response = requests.post(other.STANDUP_START_URL, json={'token': user_token, 'channel_id': 0, 'length': 10})
     assert response.status_code == 400
 
 def test_unauthorised_member(clear_store, create_user, create_user2):
     user_token = create_user['token']
     user_token_2 = create_user2['token']
     channel_id = requests.post(other.CHANNELS_CREATE_URL, json={'token': user_token, 'name': 'My Channel!', 'is_public': True}).json()['channel_id']
-    response1 = requests.post(other.STANDUP_START_URL, json={'token': user_token_2, 'channel_id': channel_id, 'length': 30})
+    response1 = requests.post(other.STANDUP_START_URL, json={'token': user_token_2, 'channel_id': channel_id, 'length': 10})
     assert response1.status_code == 403
     
