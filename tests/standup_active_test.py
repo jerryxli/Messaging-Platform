28c6566a22641cf61e9eaba3d3c7a9fcde5a3d66
Jerry Li <z5362290@ad.unsw.edu.au>
2022-04-14 23:32:23 +1000

    fixed error tests

--- tests/standup_active_test.py
+++ tests/standup_active_test.py
@@ -41,7 +41,7 @@ def test_is_not_active(clear_store, create_user):
     time_start = time()
     time_finish = int(time_start + 2)
     sleep(3)
-    
+
     response = requests.get(other.STANDUP_ACTIVE_URL, params={'token': user_token, 'channel_id': channel_id})
     assert response.status_code == 200
     response_data = response.json()
@@ -49,13 +49,18 @@ def test_is_not_active(clear_store, create_user):
     
 def test_invalid_channel(clear_store, create_user):
     user_token = create_user['token']
-    response = requests.post(other.STANDUP_START_URL, json={'token': user_token, 'channel_id': 0, 'length': 10})
+    channel_id = requests.post(other.CHANNELS_CREATE_URL, json={'token': user_token, 'name': 'Channel!', 'is_public': True}).json()['channel_id']
+    requests.post(other.STANDUP_START_URL, json={'token': user_token, 'channel_id': channel_id, 'length': 2})
+
+    response = requests.get(other.STANDUP_ACTIVE_URL, params={'token': user_token, 'channel_id': 2})
     assert response.status_code == 400
 
 def test_unauthorised_member(clear_store, create_user, create_user2):
     user_token = create_user['token']
     user_token_2 = create_user2['token']
     channel_id = requests.post(other.CHANNELS_CREATE_URL, json={'token': user_token, 'name': 'My Channel!', 'is_public': True}).json()['channel_id']
-    response1 = requests.post(other.STANDUP_START_URL, json={'token': user_token_2, 'channel_id': channel_id, 'length': 10})
-    assert response1.status_code == 403
+    requests.post(other.STANDUP_START_URL, json={'token': user_token, 'channel_id': channel_id, 'length': 2})
+
+    response = requests.get(other.STANDUP_ACTIVE_URL, params={'token': user_token_2, 'channel_id': channel_id})
+    assert response.status_code == 403
     
